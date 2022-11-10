0aa7802c072417b519074afe9b2799bc1f927060
Muhammad Azmi <117878433+Kangkarungs@users.noreply.github.com>
2022-11-10 10:08:12 +0700

    Update Activate.sh

--- Activate.sh
+++ Activate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#BY Brayan-Villa 10/06/2022
+#BY Kangkarungs 2022
 Prnt(){ printf $1; };
 Slp(){ sleep $1; };
 SshC(){ sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost ''$1''; };
